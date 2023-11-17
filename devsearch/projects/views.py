b19c3943aa9bfbb7b2006990047c43cc0c2f6967
Long Tran <55062094+milotr@users.noreply.github.com>
2023-11-17 11:23:46 +0700

    Add html files and render them in views.py
    Add html files and render them in views.py

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse("Here are our products")
-
+    return render(request, 'projects/projects.html')
 def project(request, pk):
-    return HttpResponse("Single project"+" "+str(pk))
-
+    return render(request, 'projects/single-projects.html')
