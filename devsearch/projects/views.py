7b04af50d22dd741f55abdb678a00334489ca1e6
Long Tran <55062094+milotr@users.noreply.github.com>
2023-11-17 12:37:02 +0700

    Dynamic URL Routing
    Add the dynamic URL in urls.py and views.py

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+
+{'id': '1', 'title': 'Ecommerce Website', 'description': 'Fully functional ecommerce website' },
+
+{ 'id': '2', 'title': 'Portfolio Website', 'description': 'A personal website to write articles and display work' },
+
+{'id': '3', 'title': 'Social Network', 'description': 'An open source project built by the community' }
+
+]
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = "Hello, you are in the projects page"
+    context = {
+        'message' : msg,
+        'projectList' : projectsList,
+    }
+    return render(request, 'projects/projects.html', context)
+
 def project(request, pk):
-    return render(request, 'projects/single-projects.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    context = {
+        'project':projectObj,
+    }
+    return render(request, 'projects/single-projects.html', context)
