bca3c2b75629d9b0b15f46eda932f64efdf6c319
Long Tran <55062094+milotr@users.noreply.github.com>
2023-12-01 12:41:41 +0700

    Add CRUD operation
    Create Read Update Delete

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>', views.project, name='project'),
     path('create-project/', views.createProject, name='createProject'),
+    path('update-project/<str:pk>', views.updateProject, name='updateProject'),
+    path('delete-project/<str:pk>', views.deleteProject, name='deleteProject'),
 ]
