533a5b0f2245fbdf000334b4cf3b4f0113818636
Long Tran <55062094+milotr@users.noreply.github.com>
2023-12-01 11:33:58 +0700

    Add models and display data on views
    Add models and display data on views

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-projectsList = [
-
-{'id': '1', 'title': 'Ecommerce Website', 'description': 'Fully functional ecommerce website' },
-
-{ 'id': '2', 'title': 'Portfolio Website', 'description': 'A personal website to write articles and display work' },
-
-{'id': '3', 'title': 'Social Network', 'description': 'An open source project built by the community' }
-
-]
 def projects(request):
-    msg = "Hello, you are in the projects page"
+    projects = Project.objects.all()
     context = {
-        'message' : msg,
-        'projectList' : projectsList,
+        'projects' : projects,
     }
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     context = {
-        'project':projectObj,
+        'projectObj' : projectObj,
     }
     return render(request, 'projects/single-projects.html', context)
