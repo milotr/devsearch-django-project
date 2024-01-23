c7300be33c8d804e80a90c918bf048dcb9eabf7a
Long Tran <55062094+milotr@users.noreply.github.com>
2024-01-23 14:47:55 +0700

    adding static files and configurations to django
    adding static files and configurations to django

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -15,12 +15,12 @@ def project(request, pk):
     context = {
         'projectObj' : projectObj,
     }
-    return render(request, 'projects/single-projects.html', context)
+    return render(request, 'projects/single-project.html', context)
 
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -33,7 +33,7 @@ def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=project)
+        form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
