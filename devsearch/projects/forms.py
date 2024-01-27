dc4ed07d6a19948d2fac54b29f05951b654dbe0e
Long Tran <55062094+milotr@users.noreply.github.com>
2024-01-27 15:16:38 +0700

    Install the theme
    Install the theme to the projects, single_project and forms

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,7 +1,20 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'featured_image', 'description',
+                'demo_link', 'source_link', 'tags']
+        # adding class for styling in forms
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(), 
+        }
+    #another way of adding class for styling in forms
+    def __init__(self,*args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        # this works but repetitive
+        # self.fields['title'].widget.attrs.update({'class': 'input', "placeholder": 'Add title',etc})
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
