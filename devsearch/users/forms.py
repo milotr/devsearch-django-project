0a8e711fb0b1b2fe4d6f0722497288cb46fdbe29
Long Tran <55062094+milotr@users.noreply.github.com>
2024-02-21 10:37:39 +0700

    update forms.py

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -6,3 +6,6 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
+        labels = {
+            'first_name':'Name'
+        }
