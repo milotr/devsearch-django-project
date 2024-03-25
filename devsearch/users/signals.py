e64a2ecfc79e97aadb806a5e53ea2cd06143e9ef
Long Tran <55062094+milotr@users.noreply.github.com>
2024-03-25 14:14:12 +0700

    Welcome email for new users
    Add send_email function to signals.py for new users

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile, User
+from django.core.mail import send_mail
+from django.conf import settings
 
 #@receiver(post_save, sender=Profile)    
 def createProfile(sender, instance, created, **kwargs):
@@ -13,6 +15,19 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
+
+
+
 
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
