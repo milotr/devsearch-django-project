2544383f0d2b129503167576eae6284183a42cc2
Long Tran <55062094+milotr@users.noreply.github.com>
2024-03-28 14:01:49 +0700

    Nested Serializer and Serializer Method Fields

--- devsearch/api/serializers.py
+++ devsearch/api/serializers.py
@@ -1,7 +1,36 @@
 from rest_framework import serializers
-from projects.models import Project
+from projects.models import Project, Tag, Review
+from users.models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+    #SerializerMethodField 
+    reviews =  serializers.SerializerMethodField()
+    
     class Meta:
         model = Project
         fields = '__all__'
+
+    #SerializerMethodField 
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
+    
+
